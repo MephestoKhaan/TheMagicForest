0fe2ad9c9b310e86e9718311a7b5a6c66b9693cf
LucaMefisto <luca@mefistofiles.com>
2017-07-22 15:44:26 +0100

    bringing post processing

---
+++ Assets/_Scripts/ActorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : Photon.PunBehaviour
+{
+    public void PlaceVoiceAtCharacter(int index)
+    {
+
+    }
+
+    public void GiveItemWithIndex(int index)
+    {
+
+    }
+}
