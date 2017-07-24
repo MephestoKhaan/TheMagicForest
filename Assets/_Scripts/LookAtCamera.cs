4d93827b99c3b93de05f91159370fa62c12d0344
LucaMefisto <luca@mefistofiles.com>
2017-07-24 14:47:32 +0100

    added guardians and masks

---
+++ Assets/_Scripts/LookAtCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    void Update()
+    {
+        if (Camera.main != null)
+        {
+            Vector3 clampedDirection = Vector3.ProjectOnPlane(Camera.main.transform.position - this.transform.position, Vector3.up).normalized;
+            this.transform.rotation = Quaternion.LookRotation(clampedDirection, Vector3.up);
+        }
+    }
+}
